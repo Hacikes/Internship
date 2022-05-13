5620371e8e6e8cbc66f72785e0112121c83d8d6e
Hacikes <volna475@mail.ru>
2022-05-13 18:52:29 +0300

    -_-

---
+++ Практическое задание 3 итерация/Function.js
@@ -0,0 +1,12 @@
+
+// Function Declaration
+function greet(name) {
+    console.log('Привет - ', name)
+}
+
+const greet2 = function greet2(name) {
+    console.log('Привет ещё раз, ', name)
+}
+
+greet('Лена')
+greet2('Лена')
